056093efd3efcee1615e0e3e6ff6dfd2dd19b3c2
moegitree <shchenzhiyuan@sohu.com>
2021-05-16 23:04:02 +0800

    add gui using pyqt5

--- code/Bid.py
+++ code/Bid.py
@@ -6,11 +6,17 @@ from pynput.mouse import Button as m_Button
 from pynput.mouse import Controller as m_Controller
 from pynput.keyboard import Key as k_Key
 from pynput.keyboard import Controller as k_Controller
+from PyQt5.QtCore import QThread, QObject, pyqtSignal, QTime
 
 import ImageProcess as ip
 
-class bidevent():
+class bidevent(QObject):
+    signal_firstbidPrice = pyqtSignal(str)
+    signal_secondbidPrice = pyqtSignal(str)
+    signal_buttonCoordinate = pyqtSignal(list, list)
+
     def __init__(self, monitor_num):
+        super().__init__()
         self.monitor_num = monitor_num
         self.buttons = [
                     {   "name": "offer_button",
@@ -31,11 +37,28 @@ class bidevent():
                     "secondbid_price":  1000
             }   
 
+    def bidevent_initialize(self):
+        self.setFirstbid_price("300")
+        self.setSecondbid_price("1000")
+
+    def setMonitor_num(self, n):
+        self.monitor_num = n
+        print(self.monitor_num)
+
+    def setFirstbid_price(self, value):
+        self.bidprice["firstbid_price"] = value
+        self.signal_firstbidPrice.emit(value)        
+
+    def setSecondbid_price(self, value):
+        self.bidprice["secondbid_price"] = value
+        self.signal_secondbidPrice.emit(value)
+
     def buttonrecognize(self):  #for callback
         t = ip.button_thread(self.monitor_num, self.buttons[0:2])
         t.start()
         t.join()
         print([self.buttons[i]["location"] for i in range(len(self.buttons))])
+        self.signal_buttonCoordinate.emit([self.buttons[i]["location"] for i in range(len(self.buttons))], [1,1,0])
 
     def firstbid(self):  #for callback
         mouse = m_Controller()
@@ -61,6 +84,7 @@ class bidevent():
         t = ip.button_thread(self.monitor_num, [self.buttons[2]])
         t.start()
         t.join()
+        self.signal_buttonCoordinate.emit([self.buttons[i]["location"] for i in range(len(self.buttons))], [0,0,1])
 
         mouse.position = (self.buttons[2]["location"][0]+200, self.buttons[2]["location"][1]-100)   # if button location not found, exit script with error 
         mouse.press(m_Button.left)
@@ -106,9 +130,18 @@ class bidevent():
         mouse.release(m_Button.left)
 
 
-class timer(threading.Thread):
+class bidtimer(QThread):
+    signal_initialTime = pyqtSignal(QTime)
+    signal_firstbidTime = pyqtSignal(QTime)
+    signal_firstsubmitTime = pyqtSignal(QTime)
+    signal_secondbidTime = pyqtSignal(QTime)
+    signal_secondsubmitTime = pyqtSignal(QTime)
+
+    signal_currentTime = pyqtSignal(str)
+    signal_secsToNextOp = pyqtSignal(int)
+
     def __init__(self, bid):
-        threading.Thread.__init__(self)
+        super().__init__()  # initial QThread
         self.bidobj = bid
         self.timetriggers = {
                     "initial_time": [None, self.bidobj.buttonrecognize, True],      #inital_time,
@@ -118,36 +151,48 @@ class timer(threading.Thread):
                     "secondsubmit_time": [None, self.bidobj.secondsubmit, True]     #secondsubmit_time
                 }  
         self.timetriggers_list = None     
-        self.setInitialTime("12:00:00")
-        self.setFirstbidTime("12:00:00")
-        self.setFirstsubmitTime("12:00:00")
-        self.setSecondbidTime("12:00:00")
-        self.setSecondsubmitTime("12:00:00")
 
-    def setInitialTime(self, initial_time):
+    def time_initialize(self):
+        self.setInitialTime(QTime(12,0,0)) # initialize to 12:00:00
+        self.setFirstbidTime(QTime(12,0,0))
+        self.setFirstsubmitTime(QTime(12,0,0))
+        self.setSecondbidTime(QTime(12,0,0))
+        self.setSecondsubmitTime(QTime(12,0,0))
+
+    def setInitialTime(self, initial_qtime):
+        initial_time = initial_qtime.toString("hh:mm:ss")
         t = datetime.datetime.strptime(initial_time, "%H:%M:%S")
         self.timetriggers["initial_time"][0] = t
         self.timetriggers_list = list(self.timetriggers.values())
+        self.signal_initialTime.emit(initial_qtime)
 
-    def setFirstbidTime(self, firstbid_time):
+    def setFirstbidTime(self, firstbid_qtime):
+        firstbid_time = firstbid_qtime.toString("hh:mm:ss")
         t = datetime.datetime.strptime(firstbid_time, "%H:%M:%S")
         self.timetriggers["firstbid_time"][0] = t
         self.timetriggers_list = list(self.timetriggers.values())
+        self.signal_firstbidTime.emit(firstbid_qtime)
 
-    def setFirstsubmitTime(self, firstsubmit_time):
+    def setFirstsubmitTime(self, firstsubmit_qtime):
+        firstsubmit_time = firstsubmit_qtime.toString("hh:mm:ss")
         t = datetime.datetime.strptime(firstsubmit_time, "%H:%M:%S")
         self.timetriggers["firstsubmit_time"][0] = t
         self.timetriggers_list = list(self.timetriggers.values())
+        self.signal_firstsubmitTime.emit(firstsubmit_qtime)
 
-    def setSecondbidTime(self, secondbid_time):
+    def setSecondbidTime(self, secondbid_qtime):
+        secondbid_time = secondbid_qtime.toString("hh:mm:ss")
         t = datetime.datetime.strptime(secondbid_time, "%H:%M:%S")
         self.timetriggers["secondbid_time"][0] = t
         self.timetriggers_list = list(self.timetriggers.values())
-    
-    def setSecondsubmitTime(self, secondsubmit_time):
+        self.signal_secondbidTime.emit(secondbid_qtime)
+
+    def setSecondsubmitTime(self, secondsubmit_qtime):
+        secondsubmit_time = secondsubmit_qtime.toString("hh:mm:ss")
         t = datetime.datetime.strptime(secondsubmit_time, "%H:%M:%S")
         self.timetriggers["secondsubmit_time"][0] = t
         self.timetriggers_list = list(self.timetriggers.values())
+        self.signal_secondsubmitTime.emit(secondsubmit_qtime)
 
     def run(self):
         for i in range(len(self.timetriggers_list)):
@@ -155,7 +200,11 @@ class timer(threading.Thread):
             cont = True
             while cont:
                 time_now = datetime.datetime.now()
-                print(time_now.strftime('%H:%M:%S.%f'), (self.timetriggers_list[i][0]-time_now).seconds)
+                time_now_str = time_now.strftime('%H:%M:%S')
+                secs = (self.timetriggers_list[i][0]-time_now).seconds
+                print(time_now.strftime('%H:%M:%S.%f'), secs)
+                self.signal_currentTime.emit(time_now_str)
+                self.signal_secsToNextOp.emit(secs)
 
                 if (self.timetriggers_list[i][0] - time_now).seconds == 0 and self.timetriggers_list[i][2]:
                     self.timetriggers_list[i][2] = False
---
+++ code/main.py
@@ -0,0 +1,147 @@
+from PyQt5.QtCore import QThread, QTime, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIntValidator
+
+from ui import *
+import Bid
+import datetime
+import sys
+
+def datetime2Qtime(dt):
+    ftime = dt.strftime("%H:%M:%S")
+    hour = dt.strftime("%H")
+    minute = dt.strftime("%M")
+    second = dt.strftime("%S")
+
+    return ftime, int(hour), int(minute), int(second)
+
+class showGUI(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        monitor_num = 2         # 1 - primary monitor; 2 - secondary monitor
+        bid = Bid.bidevent(monitor_num)   
+        self.timer = Bid.bidtimer(bid)
+
+        # connect gui time button to slot
+        self.setInitialTime.pressed.connect(lambda: self.timer.setInitialTime(self.editInitialTime.time()))
+        self.setFirstBidTime.pressed.connect(lambda: self.timer.setFirstbidTime(self.editFirstBidTime.time()))
+        self.setFirstSubmitTime.pressed.connect(lambda: self.timer.setFirstsubmitTime(self.editFirstSubmitTime.time()))
+        self.setSecondBidTime.pressed.connect(lambda: self.timer.setSecondbidTime(self.editSecondBidTime.time()))
+        self.setSecondSubmitTime.pressed.connect(lambda: self.timer.setSecondsubmitTime(self.editSecondSubmitTime.time()))
+
+        # connect signal from timer to gui lable slot func      
+        self.timer.signal_initialTime.connect(lambda x=self.timer.signal_initialTime: self.showTime(self.initialTime, self.editInitialTime, x))
+        self.timer.signal_firstbidTime.connect(lambda x=self.timer.signal_firstbidTime: self.showTime(self.firstBidTime, self.editFirstBidTime, x))
+        self.timer.signal_firstsubmitTime.connect(lambda x=self.timer.signal_firstsubmitTime: self.showTime(self.firstSubmitTime, self.editFirstSubmitTime, x))
+        self.timer.signal_secondbidTime.connect(lambda x=self.timer.signal_secondbidTime: self.showTime(self.secondBidTime, self.editSecondBidTime, x))
+        self.timer.signal_secondsubmitTime.connect(lambda x=self.timer.signal_secondsubmitTime: self.showTime(self.secondSubmitTime, self.editSecondSubmitTime, x))
+
+        # restrict LineEdit can only input num
+        self.editFirstBidValue.setValidator(QIntValidator(0, 65535))
+        self.editSecondBidValue.setValidator(QIntValidator(0, 65535))
+
+        # connect gui value button to slot
+        self.setFirstBidValue.pressed.connect(lambda: self.timer.bidobj.setFirstbid_price(self.editFirstBidValue.text()))
+        self.setSecondBidValue.pressed.connect(lambda: self.timer.bidobj.setSecondbid_price(self.editSecondBidValue.text()))
+
+        # connect signal from bid to gui label slot func
+        self.timer.bidobj.signal_firstbidPrice.connect(self.showValue_firstBid)
+        self.timer.bidobj.signal_secondbidPrice.connect(self.showValue_secondBid)
+
+        # comboBox 
+        self.comboBox.setCurrentIndex(monitor_num-1)
+        self.comboBox.currentIndexChanged.connect(lambda: self.timer.bidobj.setMonitor_num(self.comboBox.currentIndex()+1))
+
+        # show auction button location
+        self.timer.bidobj.signal_buttonCoordinate.connect(self.showCoordinate)
+
+        # connect test mode button
+        self.testModeButton.pressed.connect(self.setTestMode)
+
+        # connect default mode button
+        self.defaultModeButton.pressed.connect(self.setDefault)
+
+        # connect time signal
+        self.timer.signal_currentTime.connect(self.clockLabel)
+        self.timer.signal_secsToNextOp.connect(self.countdownLabel)
+
+        self.timer.bidobj.bidevent_initialize()
+        self.timer.time_initialize()
+
+        self.timer.start()
+
+    def showTime(self, label, edit, t):
+        label.setText(t.toString("hh:mm:ss"))
+        edit.setTime(t)
+
+    def showValue_firstBid(self, v):
+        self.firstBidValue.setText(v)
+        self.editFirstBidValue.setText(v)
+
+    def showValue_secondBid(self, v):
+        self.secondBidValue.setText(v)
+        self.editSecondBidValue.setText(v)
+
+    def showCoordinate(self, l, f):
+        if f[0] == 1:
+            if (l[0][0] is not None) and (l[0][1] is not None): 
+                self.addButtonCoordinate.setStyleSheet("color:black")
+                self.addButtonCoordinate.setText( "({0}, {1})".format(str(l[0][0]), str(l[0][1])) )
+            else:
+                self.addButtonCoordinate.setStyleSheet("color:red")
+                self.addButtonCoordinate.setText( "未能识别" )
+        if f[1] == 1:
+            if (l[1][0] is not None) and (l[1][1] is not None): 
+                self.offerButtonCoordinate.setStyleSheet("color:black")
+                self.offerButtonCoordinate.setText( "({0}, {1})".format(str(l[1][0]), str(l[1][1])) )
+            else:
+                self.offerButtonCoordinate.setStyleSheet("color:red")
+                self.offerButtonCoordinate.setText("未能识别")
+        if f[2] == 1:
+            if (l[2][0] is not None) and (l[2][1] is not None): 
+                self.confirmButtonCoordinate.setStyleSheet("color:black")
+                self.confirmButtonCoordinate.setText( "({0}, {1})".format(str(l[2][0]), str(l[2][1])) )
+            else:
+                self.confirmButtonCoordinate.setStyleSheet("color:red")
+                self.confirmButtonCoordinate.setText("未能识别")
+
+    def setTestMode(self):
+        now = QTime.currentTime()
+        trigger = now.addSecs(6)
+        self.timer.setInitialTime(trigger)
+
+        trigger = now.addSecs(6*2)
+        self.timer.setFirstbidTime(trigger)
+
+        trigger = now.addSecs(6*3)
+        self.timer.setFirstsubmitTime(trigger)
+
+        trigger = now.addSecs(6*4)
+        self.timer.setSecondbidTime(trigger)
+        
+        trigger = now.addSecs(6*5)
+        self.timer.setSecondsubmitTime(trigger)  
+
+    def setDefault(self):
+        self.timer.setInitialTime(QTime(12,0,0))
+        self.timer.setFirstbidTime(QTime(12,0,0))
+        self.timer.setFirstsubmitTime(QTime(12,0,0))
+        self.timer.setSecondbidTime(QTime(12,0,0))
+        self.timer.setSecondsubmitTime(QTime(12,0,0))
+        self.timer.bidobj.setFirstbid_price("300")
+        self.timer.bidobj.setSecondbid_price("1000")
+
+    def clockLabel(self, s):
+        self.currentTime.setText(s)
+
+    def countdownLabel(self, i):
+        self.countDown.setText("距下次动作还有 {0} 秒".format(i))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = showGUI()
+  
+    myWin.show()
+    sys.exit(app.exec_())
